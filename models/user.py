6b1d083152b6ab15f079ada59aa4d16458342614
timothytran051 <timothytran051@gmail.com>
2025-04-25 16:14:55 -0700

    token exchange function

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 class MicrosoftUser(Base): #schema for user table
     __tablename__ = "users"
     
-    id = Column(String, primary_key=True, index=True) # Microsoft ID
+    id = Column(String, primary_key=True, index=True) 
     email = Column(String, unique=True, index=True, nullable=False)
     name = Column(String, nullable=True)
     
