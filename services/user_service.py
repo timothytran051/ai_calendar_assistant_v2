1376d28e8930a2bbf98f7dd6d4c5bc9005264870
timothytran051 <timothytran051@gmail.com>
2025-05-28 09:36:13 -0700

    updated decode function to take refresh token and expiration time

--- services/user_service.py
+++ services/user_service.py
@@ -1,15 +1,19 @@
 import requests
 from db.db import db
+from datetime import datetime, timedelta
 
-def get_user_info(access_token):    
+def get_user_info(token_data):    
     graph_url = "https://graph.microsoft.com/v1.0/me"
+    access_token = token_data["access_token"]
     data = requests.get(graph_url, headers={"Authorization": f"Bearer {access_token}"}) #uses access token to retrieve data from microsoft graph /me
     response_data = data.json() #includes user table data
     user_data = {
         "_id": response_data["id"], #retrieves id
         "displayName": response_data.get("displayName"), #get makes parameter optional
         "email": response_data.get("mail"),
-        "username": response_data.get("userPrincipalName")
+        "username": response_data.get("userPrincipalName"),
+        "refresh_token": token_data.get("refresh_token"),
+        "expires_in": token_data.get("expires_in")
     }
     return user_data
     
@@ -19,5 +23,8 @@ async def upsert_user(user_data): #upsert function (insert or update)
     await users_collection.update_one( 
         {"_id": user_data["_id"]}, #grabs user id from table, if found then mongo will go to next step, if not then it will create an _id
         {"$set": user_data}, #updates or inserts user data
+        # {"refresh_token": refresh}, #refresh token for when the users access token expires. 
+        #                             #this token will be used instead to keep the user from having to log in again
+        # {"token_expiry": datetime.utcnow() + timedelta(seconds = expire)}, #token expiration translated to a 
         upsert=True #update or insert
     )
